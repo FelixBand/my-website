4dee0a4b380520cf42b0e760ab323af9e5194360
FelixBand <felixband@gmail.com>
2025-02-04 11:09:36 +0100

    fix bind ip

--- mailbox.py
+++ mailbox.py
@@ -25,4 +25,4 @@ def send_message():
     return "Message sent successfully!"
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000)  # Internal only, managed by Gunicorn
+    app.run(host="0.0.0.0", port=5000)  # Internal only, managed by Gunicorn
